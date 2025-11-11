0822883ce0142ba8c1d69f3d1d6b19f0bed5d7f8
Eyad izrafeel <eyadizrafeel@Ranis-MacBook-Pro.local>
2025-11-11 18:49:05 +0200

    backend

---
+++ backend/shema/Store.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const storeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  description: { type: String },
+  items: [{ type: mongoose.Schema.Types.ObjectId, ref: "Item" }],
+}, { timestamps: true });
+
+const Store = mongoose.model("Store", storeSchema);
+export default Store;

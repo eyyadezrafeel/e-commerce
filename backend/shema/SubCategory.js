316c9674b9c27e55b3db501cfe74949c36dd3e45
Eyad izrafeel <eyadizrafeel@Ranis-MacBook-Pro.local>
2025-11-11 18:54:03 +0200

    backend

---
+++ backend/shema/SubCategory.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const subCategorySchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  parentCategory: { type: mongoose.Schema.Types.ObjectId, ref: "Category", required: true },
+});
+
+const SubCategory = mongoose.model("SubCategory", subCategorySchema);
+export default SubCategory;
